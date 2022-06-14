111ffaad0744b37a2ada33093ff60394e32b407f
Deepak <deepchaurasiya1997@gmail.com>
2022-06-14 22:44:20 +0530

    This is second commit of this project

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 setInterval(() => {
   let hours = document.getElementById("hours");
   let seconds = document.getElementById("seconds");
-  let ampm = document.getElementById("ampm");
   let minutes = document.getElementById("minutes");
+  let ampm = document.getElementById("ampm");
+
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
+
+  let hr_dot = document.querySelector("hr_dot");
+  let min_dot = document.querySelector("min_dot");
+  let sec_dot = document.querySelector("sec_dot");
 
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
@@ -19,8 +27,22 @@ setInterval(() => {
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  hours.innerHTML = h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
+  hours.innerHTML = h + "<br><span>Hours</span>";
+  minutes.innerHTML = m + "<br><span>Minutes</span>";
+  seconds.innerHTML = s + "<br><span>Seconds</span>";
   ampm.innerHTML = am;
+  hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+  // 12hour clock
+  mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+  // 60 minutes clock
+  ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+  // 60 seconds clock
+
+  hr_dot.style.transform = `rotate(${h * 30}deg)`;
+  // 360/12=30
+  min_dot.style.transform = `rotate(${m * 6}deg)`;
+  // 360/60=6
+
+  sec_dot.style.transform = `rotate(${s * 6}deg)`;
+  // 360/60=6
 });
